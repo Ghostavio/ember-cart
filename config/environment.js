f37b67bcb968bd285742e9dc127ea459454df8b1
Ghostavio <gustavo.anacleto@gmail.com>
2016-04-22 03:33:51 -0300

    added contentSecurityPolicy to handle everything + api

--- config/environment.js
+++ config/environment.js
@@ -13,6 +13,16 @@ module.exports = function(environment) {
       }
     },
 
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self'",
+      'font-src': "'self'",
+      'connect-src': "'self' *",
+      'img-src': "'self'",
+      'style-src': "'self'",
+      'media-src': "'self'"
+    },
+
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
