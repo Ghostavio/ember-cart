66acf77a795912e58e73d78366ee0b6157dad02f
Ghostavio <gustavo.anacleto@gmail.com>
2016-04-22 12:05:30 -0300

    added missing semicolon

--- app/helpers/to-money.js
+++ app/helpers/to-money.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export function toMoney(params) {
   let total;
   if (~navigator.userAgent.indexOf('Safari')) {
-    total = `$${Number(params[0]).toFixed(2).replace(/\B(?=(\d{3})+\b)/g, ",")}`
+    total = `$${Number(params[0]).toFixed(2).replace(/\B(?=(\d{3})+\b)/g, ",")}`;
   } else {
     total = `$${Number(params[0]).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})}`;
   }
