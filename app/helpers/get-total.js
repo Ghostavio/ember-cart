f788261dbcb5664def18945554c68d58f29a9318
Ghostavio <gustavo.anacleto@gmail.com>
2016-04-22 11:16:02 -0300

    fixed safari's issue with toLocaleString()

--- app/helpers/get-total.js
+++ app/helpers/get-total.js
@@ -2,7 +2,14 @@ import Ember from 'ember';
 
 export function getTotal(params) {
   const calc = Number(params[0].substr(1).replace(',', '')) * Number(params[1]);
-  return `$${calc.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})}`;
+  let total;
+  if (~navigator.userAgent.indexOf('Safari')) {
+    total = `$${Number(calc).toFixed(2).replace(/\B(?=(\d{3})+\b)/g, ",")}`;
+  } else {
+    total = `$${Number(calc).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})}`;
+  }
+
+  return total;
 }
 
 export default Ember.Helper.helper(getTotal);
--- app/helpers/to-money.js
+++ app/helpers/to-money.js
@@ -1,7 +1,14 @@
 import Ember from 'ember';
 
 export function toMoney(params) {
-  return `$${Number(params[0]).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})}`;
+  let total;
+  if (~navigator.userAgent.indexOf('Safari')) {
+    total = `$${Number(params[0]).toFixed(2).replace(/\B(?=(\d{3})+\b)/g, ",")}`
+  } else {
+    total = `$${Number(params[0]).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})}`;
+  }
+
+  return total;
 }
 
 export default Ember.Helper.helper(toMoney);
