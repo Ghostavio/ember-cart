452942acaae95aba7e36233cafcec9c8e740ac3b
Ghostavio <gustavo.anacleto@gmail.com>
2016-04-24 22:03:45 -0300

    fixed typo

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import RESTAdapter from 'ember-data/adapters/rest';
 
 export default RESTAdapter.extend({
-  // well we can use `host` because ember-data would add /products to
+  // well we can't use `host` because ember-data would add /products to
   // the end of the url, so we did a little workaround with buildURL :)
   // host: 'http://beta.json-generator.com/api/json/get/4kiDK7gxZ',
   buildURL: () => {
